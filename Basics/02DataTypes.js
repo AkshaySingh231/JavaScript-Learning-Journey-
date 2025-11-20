4bcca6880cb85141c0fb35e4dd8991046bd9791c
--get <--get>
2025-11-21 00:27:30 +0530

    Data types

--- Basics/02DataTypes.js
+++ Basics/02DataTypes.js
@@ -1,2 +1,12 @@
 "use strict"; // Treat all js code as newer version 
 //alert("New learning about Data types");   ------------>>>     // this will not work in node js ,it works in browser;
+
+let name = "John"; // String literal
+let age = 30; // Number literal
+let isAdmin = true; // Boolean literal
+let height; // Undefined literal
+let bigNumber = 1234567890123456789012345678901234567890n; // BigInt literal    
+let symbol = Symbol("id"); // Symbol literal
+let person = null; // Null literal
+
+console.log("Name:", name);                 // Output: Name: John  
