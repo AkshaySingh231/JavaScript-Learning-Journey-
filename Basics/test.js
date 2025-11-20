e430b0b0c0c1a5279d5d8bec4ac3995a28b62c60
--get <--get>
2025-11-20 23:46:16 +0530

    Initial commit

---
+++ Basics/test.js
@@ -0,0 +1,7 @@
+const RollNo =2301641722013; // Can't change data if Once Assign ;
+let name ="John doe"; //Can chnge data if once assign and it is a block scoped;
+var age = 21 ; // can change data if once assign and it is function scoped;
+// console.log("Roll No ",RollNo);
+// console.log("Name ",name);
+// console.log("Age ",age);
+console.table([RollNo,name,age]); 
