b8f3575188e7f301f92c98d0a66180fa61612ad3
--get <--get>
2025-11-21 00:56:53 +0530

    Add conversion operations and type logging for variables

---
+++ Basics/03ConversionOperation.js
@@ -0,0 +1,12 @@
+let score =33
+console.log(typeof score);
+console.log(typeof (score));
+
+let value =String(score);
+console.log(typeof value);
+
+let val =null ;
+console.log(typeof val);
+
+let val1 =Number("123abc");
+console.log(val1);
