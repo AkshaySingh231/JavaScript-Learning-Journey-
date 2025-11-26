667dd69843255e74094f9b16a9778ecbe2a5d54b
--get <--get>
2025-11-26 11:03:25 +0530

    Add string manipulation and type logging examples

---
+++ Basics/string.js
@@ -0,0 +1,28 @@
+const name = "Akshay";
+const repoCount = 60;
+
+console.log(name + repoCount + "Value");
+console.log(`Hello my name is ${name} my repo count is ${repoCount}`);
+
+const firstName = new String("Singh");
+// console.log(firstName.substring(0,3));
+
+console.log(typeof firstName);
+console.log(firstName.charAt(2));
+
+console.log(firstName.indexOf('g'));
+console.log(firstName.toUpperCase());
+console.log(firstName.replace("Singh","Pratap"));
+console.log(`${firstName.valueOf()}`);
+
+
+let score =33 ;
+console.log(typeof score);
+console.log(`type of ${score}`);
+
+const newString = "   Akshay Singh l  ";
+console.log(newString.valueOf());
+console.log(newString.trim());
+
+console.log(newString.split(""));
+
